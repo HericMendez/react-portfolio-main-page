5c42861a009010c2bffc081a55481cf44c9535a6
HericMendez <heric.mendez00@gmail.com>
2022-04-28 18:31:16 -0300

    Created 'Home' Component

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+        <p className=" text-xl text-pink-600">Hi, my name is</p>
+        <h2 className="text-4xl sm:text-6xl font-bold text-[#ccd6f6]">
+          Heric Mendes
+        </h2>
+        <h3 className="text-2xl sm:text-5xl font-bold pb-5 text-[#8892b0]">
+          I am a Front-end Developer.
+        </h3>
+        <p className="text-[#8892b0] py-4  max-width-[700px] ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit
+          amet fringilla lacus. Integer lobortis felis quis dapibus vulputate.
+          Nulla ex nibh, maximus quis hendrerit eu, malesuada sit amet ex.
+          Pellentesque a urna dolor.
+        </p>
+        <div>
+          <button className="group outline p-3 mt-5 flex items-center hover:bg-pink-600  outline-pink-600 text-[#ccd6f6] text-bold ">
+            View my work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-4" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/placeholder-logo.png";
+import { HiOutlineMail } from "react-icons/hi";
+import { SiGmail } from "react-icons/si"
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
-    const menuClickHandler = () => setNav(!nav)
-
+  const [nav, setNav] = useState(false);
+  const menuClickHandler = () => setNav(!nav);
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
@@ -27,13 +29,46 @@ const Navbar = () => {
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/*Mobile menu*/}
-      <ul className={!nav ?'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
         <li className="py-6 text-4xl">Projects</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
+
+      {/*Social icons (side)  */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li  className="w-[160px] h-[35px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a  className="flex justify-between w-full items-center text-gray-300" href="#">Linkedin <FaLinkedin size={30} /></a>
+          </li>
+
+          <li  className="w-[160px] h-[35px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-purple-800">
+            <a  className="flex justify-between w-full items-center text-gray-300" href="#">Github <FaGithub size={30} /></a>
+          </li>
+
+          <li  className="w-[160px] h-[35px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-600">
+            <a  className="flex justify-between w-full items-center text-gray-300" href="#">Gmail <SiGmail size={30} /></a>
+          </li>
+
+          <li  className="w-[160px] h-[35px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600">
+            <a  className="flex justify-between w-full items-center text-gray-300" href="#">Whatsapp <FaWhatsapp size={30} /></a>
+          </li>
+
+          <li  className="w-[160px] h-[35px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600">
+            <a  className="flex justify-between w-full items-center text-gray-300" href="#">Resume<BsFillPersonLinesFill size={30} /></a>
+          </li>
+         
+         
+        </ul>
+      </div>
     </div>
   );
 };
